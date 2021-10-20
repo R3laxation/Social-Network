89dc09940cbb7fe5a3635042cff52d737e30eece
Konstantin <memories@bk.ru>
2021-10-20 11:01:15 +0600

    release v1

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <header className='header'>
+            <img
+                src="https://img2.freepng.ru/20180606/cwk/kisspng-tesla-motors-car-tesla-model-3-electric-vehicle-tesla-logo-5b181af2ce9451.1195702715283064188462.jpg"
+                alt="logo"/>
+        </header>
+    )
+}
